93ec19d3546ac76b6007dff9699734aacc96f9fd
SOWAIBA NAZ <167256849+sowaibathecoder@users.noreply.github.com>
2024-09-13 08:30:08 +0500

    Add files via upload
    This is a npx based todo list from typescript, node.js, and package inquirer and chalk.

---
+++ main.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.magentaBright("\n\t\t\t\t\t *****TODO LIST***** \t\t\t\t\t\n"));
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQues = await inquirer.prompt([
+        {
+            name: "firstQues",
+            type: "input",
+            message: chalk.yellowBright("What would you like to add in your Todos?"),
+        },
+        {
+            name: "secondQues",
+            type: "confirm",
+            message: chalk.yellowBright("Would you like to add more in your todos?"),
+        }
+    ]);
+    todos.push(todoQues.firstQues);
+    console.log(chalk.bold.greenBright("\nAdded in your todo list is: \n"));
+    for (let i = 0; i < todos.length; i++) {
+        console.log(`${i + 1}. ${chalk.cyanBright(todos[i])}`);
+    }
+    console.log("");
+    condition = todoQues.secondQues;
+}
+console.log(chalk.yellow("HAVE A NICE DAY!"));
